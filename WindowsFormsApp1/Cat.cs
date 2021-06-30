efd1f589d48a2dc06617932bae5350f3072a380b
JR Dimaandal <jrdimaandal@gmail.com>
2021-06-30 15:03:02 +0800

    test
    test

---
+++ WindowsFormsApp1/Cat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+
+    public class Cat
+    {
+        // Field name 
+        private string name;
+        // Field color 
+        private string color;
+
+        public string Name
+        {
+            // Getter of the property "Name" 
+            get
+            {
+                return this.name;
+            }
+            // Setter of the property "Name" 
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Color
+        {
+            // Getter of the property "Color" 
+            get
+            {
+                return this.color;
+            }
+            // Setter of the property "Color" 
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        // Default constructor 
+        public Cat()
+        {
+            this.name = "Unnamed";
+            this.color = "gray";
+        }
+
+
+        // Constructor with parameters 
+        public Cat(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+
+        // Method SayMiau 
+        public string SayMiau()
+        {
+            return string.Format("Cat {0} said: Miauuuuuu!", name);
+            //Console.WriteLine("Cat {0} said: Miauuuuuu!", name);
+        }
+    }
+
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cat firstCat = new Cat();
+            firstCat.Name = "Tony";
+
+            label1.Text= firstCat.SayMiau();
+
+            Cat secondCat = new Cat("Pepy", "red");
+
+            label2.Text = secondCat.SayMiau();
+
+            label3.Text = string.Format("Cat {0} is {1}.", secondCat.Name, secondCat.Color);
+
+            //Console.WriteLine("Cat {0} is {1}.", secondCat.Name, secondCat.Color);
+
+        }
+    }
+}
